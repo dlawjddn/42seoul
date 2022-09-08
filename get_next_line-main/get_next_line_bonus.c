abb2b3053cb2f5f3016d34712636932e232619b2
dlawjddn <103355883+dlawjddn@users.noreply.github.com>
2022-09-08 11:27:25 +0900

    Update get_next_line_bonus.c

--- get_next_line-main/get_next_line_bonus.c
+++ get_next_line-main/get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*store_buf(char *buf, char *backup)
 {
